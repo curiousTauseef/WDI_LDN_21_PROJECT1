b5a637e82acabd39ecae494b1141c9fba691fc13
Jason Lai <jase_lai@hotmail.com>
2016-07-08 14:25:59 +0100

    working win condition by comparing arrays

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 //oop
 
 
-var numberOfSquares = 25;
+var numberOfSquares = 4;
 var rootOfSquares = Math.sqrt(numberOfSquares);
 var squarePixels = Math.floor( (440 - (rootOfSquares*2*5))/rootOfSquares );
+
+var gridArray = [];
+var winArray = [];
 
 //Calling this function creates the squares on the page
 function CreateSquares(){
@@ -16,6 +19,9 @@ function CreateSquares(){
     else {
       $("ol").append("<li id='" + i + "' class='square' style='width:" + squarePixels + "px; height:"+squarePixels +"px'>" + i + "</li>");
     }
+
+    gridArray.push(i); 
+
   }
 
   //Add event listeners so that clicked square swaps classes and Ids.
@@ -35,12 +41,23 @@ function CreateSquares(){
       $(this).attr("class", "blank").attr("id",blankSquareId).html(blankSquareId);
     }
 
-  });
+    winArray = [];
+    $('li').each(function (i) {
+      winArray.push($(this).attr("id"));
+    });
+    
+    console.log(winArray);
+    checkForWin();
 
+  });
 
 } // closing tag for CreateSquares
 
-
+function checkForWin() { 
+  if (gridArray.toString() === winArray.toString()) {
+    alert("You Win!");
+  }
+}
 
 
 
