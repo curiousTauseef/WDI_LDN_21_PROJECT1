5dfcc7da16e5f84adfe65ff1bad8cb7825c9a406
Jason Lai <jase_lai@hotmail.com>
2016-07-10 15:40:26 +0100

    animation and sound effects added. Timer for game in progress

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
-var numberOfSquares = 9;
-var rootOfSquares = Math.sqrt(numberOfSquares);
-var squarePixels = Math.floor( (440 - (rootOfSquares*2*5))/rootOfSquares );
+var backgroundAudio = new Audio("sounds/jazz-loop.wav");
 
+backgroundAudio.play();
+setInterval(function(){
+  backgroundAudio.play();
+  console.log("music playing");
+}, 68000);
+
+
+
+
+var numberOfSquares = 0;
 var gridArray = [];
-var winArray = [];
+var playerArray = [];
+var counter;
+// var playerTime = "";
+// var gameInPlay = false;
+var count = 10;
+
 
 $ol = $("ol");
 $lis = {};
-$startButton = $("#startButton")
+
 $easyButton = $("#easyButton")
 $normalButton = $("#normalButton")
 $hardButton = $("#hardButton")
@@ -21,28 +34,61 @@ function generateRandomColor() {
   return color;
 }
 
-$startButton.on("click", CreateSquares);
+// var timeOut = function() {
+//   setInterval(function(){ 
+//     alert("You ran out of time. The computer messes up your board"); 
+//     shuffleIds();
+//   }, 10000);
+// }
+
+$easyButton.on("click", function() {
+  if ($("li").length === 0) {
+    numberOfSquares = 9;
+    rootOfSquares = Math.sqrt(numberOfSquares);
+    squarePixels = Math.floor( (440 - (rootOfSquares*2*5))/rootOfSquares );
+    CreateSquares();
+    shuffleIds();
+  }
+});
+
+$normalButton.on("click", function() {
+  if ($("li").length === 0) {
+    numberOfSquares = 16;
+    rootOfSquares = Math.sqrt(numberOfSquares);
+    squarePixels = Math.floor( (440 - (rootOfSquares*2*5))/rootOfSquares );
+    CreateSquares();
+    shuffleIds();
+  }
+});
+
+$hardButton.on("click", function() {
+  if ($("li").length === 0) {
+    numberOfSquares = 25;
+    rootOfSquares = Math.sqrt(numberOfSquares);
+    squarePixels = Math.floor( (440 - (rootOfSquares*2*5))/rootOfSquares );
+    CreateSquares();
+    shuffleIds();
+  }
+});
+
+
 //Calling this function creates the squares on the page
 function CreateSquares(){
+
   //loop to create squares
-  for (var i = 0; i < numberOfSquares; i++) {
+    for (var i = 0; i < numberOfSquares; i++) {
 
-    var randomColor = generateRandomColor();
+      var randomColor = generateRandomColor();
 
-    if(i === 0) {
-      $ol.append("<li id='square" + i + "' class='blank' style='width:" + squarePixels + "px; height:"+squarePixels +"px;' data-index='"+i+"'>" + i + "</li>");
+      if(i === 0) {
+        $ol.append("<li id='square" + i + "' class='blank animated infinite pulse' style='width:" + squarePixels + "px; height:"+squarePixels +"px;' data-index='"+i+"'>" + i + "</li>");
+      }
+      else {
+      $ol.append("<li id='square" + i + "' class='square animated flipInY' style='width:" + squarePixels + "px; height:"+squarePixels +"px; background-color:"+randomColor+";   ' data-index='"+i+"' >" + i + "</li>");
+      }
+      gridArray.push(i);
+      gameInPlay = true;   
     }
-    else {
-      $ol.append("<li id='square" + i + "' class='square' style='width:" + squarePixels + "px; height:"+squarePixels +"px; background-color:"+randomColor+";   ' data-index='"+i+"' >" + i + "</li>");
-    }
-
-    gridArray.push(i); 
-
-    // setTimeout(function(){
-    //   shuffleIds();
-    // }, 2000)
-  }
-
 
 
   //Add event listeners so that clicked square swaps classes and Ids.
@@ -51,6 +97,9 @@ function CreateSquares(){
   $lis = $("li");
 
   $lis.on("click", function() {
+
+    var audio = new Audio("sounds/pop.wav");
+    audio.play();
 
     var clickedIndex = ($(this).index());
     var clickedSquareDataIndex = $(this).attr("data-index");
@@ -62,27 +111,31 @@ function CreateSquares(){
 
     // if(clickedIndex === blankSquareIndex+1 || clickedIndex === blankSquareIndex+gridSize || clickedIndex === blankSquareIndex-1 || clickedIndex === blankSquareIndex-gridSize) {
       
-      blankSquare.attr("class", "square").attr("data-index",clickedSquareDataIndex).attr("style", clickedStyle).html(clickedSquareDataIndex);
+      blankSquare.attr("class", "square animated flipInY").attr("data-index",clickedSquareDataIndex).attr("style", clickedStyle).html(clickedSquareDataIndex);
 
-      $(this).attr("class", "blank").attr("data-index",blankSquareDataIndex).html(blankSquareDataIndex);
+      $(this).attr("class", "blank animated infinite pulse").attr("data-index",blankSquareDataIndex).html(blankSquareDataIndex);
     // }
 
-    winArray = [];
-    $lis.each(function (i) {
-      // winArray.push($(this).index());      
-      winArray.push($(this).attr("data-index"));
+    playerArray = [];
+    $lis.each(function (i) {  
+      playerArray.push($(this).attr("data-index"));
     });
     
     checkForWin();
 
   });
-
 } // closing tag for CreateSquares
 
 function checkForWin() { 
-  console.log(winArray)
-  if (gridArray.toString() === winArray.toString()) {
+  console.log(playerArray)
+  if (gridArray.toString() === playerArray.toString()) {
     alert("You Win!");
+    clearInterval(counter);
+    var playerTime = count;
+    count = 75;
+    console.log(playerTime);
+    console.log(count);
+
   }
 }
 
@@ -125,34 +178,61 @@ function shuffleIds() {
   });
 
   resetClasses();
+  // activateTimer();
 
 }
 
 function resetClasses() {
-  $lis.attr("class", "blank");
+  $lis.attr("class", " blank animated infinite pulse");
 
   setTimeout(function(){
 
-  $lis.attr("class", "square");
-  $lis.filter("#square0").attr("class", "blank");
+  $lis.attr("class", "square animated flipInY");
+  $lis.filter("#square0").attr("class", "blank animated infinite pulse");
   
   }, 500);
 }
 
-//function to randomize Lis. 
-// $.fn.randomize = function(selector){
-//   var $elems = selector ? $(this).find(selector) : $(this).children(),
-//   $parents = $elems.parent();
 
-//   $parents.each(function(){
-//     $(this).children(selector).sort(function(){
-//       return Math.round(Math.random()) - 0.5;
 
-//     }).detach().appendTo(this);
-//   });
+function activateTimer(){
+  counter = setInterval(timer, 1000); //1000 will  run it every 1 second
 
-//   return this;
-// };
+  function timer() {
+    count = count - 1;
+    console.log(count);
+    
+    var seconds = count % 60;
+    var minutes = Math.floor(count / 60);
+    var hours = Math.floor(minutes / 60);
+    minutes %= 60;
+    hours %= 60;
+
+
+    if (seconds > 9) {
+      $("#timer").text(seconds+ " seconds remaining")
+    }
+    else if (seconds <= 9) {
+      $("#timer").text(seconds+ " seconds remaining")
+    }
+
+    if (count === 0) {
+
+      alert("You ran out of time; the computer messes up your game board!");
+      shuffleIds();
+
+      count = 10;
+
+    }
+    
+    if (count === -1) {
+      clearInterval(counter);  
+      return;
+    }
+  }
+}
+
+
 
 
 
